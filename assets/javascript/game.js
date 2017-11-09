a0700da4fa03bfe002890010bd86579604b686d6
faviana lopez <ms.favina@gmail.com>
2017-11-09 12:12:29 -0500

    fixed guessesLeft count loop and game can restart

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,44 @@ var alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 var wins = 0;
 var losses = 0;
 var guessesLeft = 15;
-var guessesMade = "";
+var guessesMade = [];
 
-//start a function when player presses a key
+//start a function when user presses a key
 document.onkeyup = function(event) {
 
-    //determine the key that player pressed
-    var player = event.key;
+    //determine/store the key that user pressed
+    var user = event.key;
+
+    //add guessed letter to the array guessesMade
+    guessesMade.push(user);
 
     //computer chooses a random letter
     var computer = alphabet[Math.floor(Math.random() * alphabet.length)];
 
-    //if statements
-    if (player === computer){
+    //if statement: If the guess of the user equals the computer letter
+    if (user === computer){
+        //user wins
         wins++;
-        confirm("You Won!!!" + userName + "Play again?");
+        alert("You Won!!! ... I guessed: "+computer);
+        confirm("Play again?");
+        //guessesLeft restarts to 15 guesses
+        guessesLeft = 15;
     }
     else{
+        //else, guessesLeft count reduces 1 value; game continues
         guessesLeft--;
+
     }
 
+    //if statement: If the guessesLeft count equals zero
     if (guessesLeft === 0){
+        //user losses
         losses++;
-        confirm("You Lost. I Guessed: " + computer + " Play again?");
+        alert("You Lost. I Guessed: "+computer)
+        confirm("Play again?");
+        //guessesLeft restarts to 15 guesses
+        guessesLeft = 15;
+        
     }
 
     //show score
@@ -37,7 +52,6 @@ document.onkeyup = function(event) {
     "<p>Guesses Left: " + guessesLeft + "</p>"+
     "<p>Guesses Made: " + computer + "</p>";
 
-
     document.getElementById("game").innerHTML = html;
-
+  
 }
